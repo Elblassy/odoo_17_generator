b7c8592b1cca9e3ba034336d26df8f0722d4e5e7
GitHub Action <action@github.com>
2025-01-09 17:40:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/elblassy/odoo17generator/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/elblassy/odoo17generator/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.elblassy.odoo17generator.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
